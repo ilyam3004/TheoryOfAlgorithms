a16500fd52cde3197a3605f534f48cb17c8c71d2
ilyam3004 <ilyamelnichuk3004@gmail.com>
2022-02-14 15:46:41 +0200

    ReleaseVersion

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace Lab1
@@ -42,75 +43,108 @@ namespace Lab1
         //------------- Random entering array -------------
         static void RandomNumbers()
         {
+            //Start input data
             Console.WriteLine("-------------------------\n" + "Enter count of columns:");
-            int columns = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
             Console.WriteLine("-------------------------\n" + "Enter count of rows:");
-            int rows = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
             Console.WriteLine("----------------------\n" + "Enter range of numbers\nFrom:");
             int from = int.Parse(Console.ReadLine());
             Console.WriteLine("To:");
             int to = int.Parse(Console.ReadLine());
-            array = new int[rows, columns];
+            array = new int[n, m];
             int min = array[0, 0];
+            string index = "";
             Random rnd = new();
-            Console.WriteLine("-------------------------\n" + "Array:"); 
-            for (int i = 0; i < rows; i++)
+            //Entering random numbers in array
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < columns; j++)
-                { 
+                for (int j = 0; j < m; j++)
+                {
                     array[i, j] = rnd.Next(from, to);
-                    Console.Write(array[i,j] + " ");
-                    if (array[i, j] < min)
-                        min = array[i, j];
                 }
-                Console.WriteLine();
             }
+            //Cheking time
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            //Main code for searching min element
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if(array[i, j] < min)
+                    {
+                        min = array[i, j];
+                        index = "[" + i + "," + j + "]";
+                    }
+                }
+            }
+            stopWatch.Stop();
             Console.WriteLine("--------------------\n" +
                               "Min element: " + min + "\n" +
+                              "Index of min element: " + index + "\n" + 
+                              "Time spent: " + stopWatch.ElapsedMilliseconds + " ms\n" +
                               "--------------------\n");
             using (StreamWriter sw = new StreamWriter(@"C:\Users\ilyam\Desktop\Навчання\TheoryOfAlgorithms\Lab1\output\output.txt", true, System.Text.Encoding.Default))
             {
-                for (int i = 0; i < rows; i++)
+                for (int i = 0; i < n; i++)
                 {
-                    for (int j = 0; j < columns; j++)
+                    for (int j = 0; j < m; j++)
                     {
-                        if(array[i, j] < min)
-                            min = array[i, j];
                         sw.Write(array[i, j] + " ");
                     }
                     sw.WriteLine();
                 }
-                sw.WriteLine("Min element of this array: " + min);
+                sw.WriteLine("Min element of this array: " + min + "\n" +
+                             "Index of min element: " + index + "\n" +
+                             "Time spent: " + stopWatch.ElapsedMilliseconds + " ms");
                 sw.WriteLine("----------------------------");
             }
         }
         //------------- Reading array from file and writing output data in file -------------
         static void ReadFromFile()
         {
+            //Input data processing
             string[] lines = File.ReadAllLines(@"C:\Users\ilyam\Desktop\Навчання\TheoryOfAlgorithms\Lab1\input\input.txt");
-            array = new int[lines.Length, lines[0].Split(' ').Length];
+            array = new int[lines.Length, lines[0].Split().Length];
             for (int i = 0; i < lines.Length; i++)
             {
-                string[] temp = lines[i].Split(' ');
+                string[] temp = lines[i].Split();
                 for (int j = 0; j < temp.Length; j++)
                 {
                     array[i, j] = int.Parse(temp[j]);
                 }
             }
             int min = array[0, 0];
+            string index = "[0,0]";
+            //Cheking time of data processing
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[0].Split().Length; j++)
+                {
+                    if(array[i, j] < min)
+                    {
+                        min = array[i, j];
+                        index = "[" + i + "," + j + "]";
+                    }
+                }
+            }
+            stopWatch.Stop();
             using (StreamWriter sw = new StreamWriter(@"C:\Users\ilyam\Desktop\Навчання\TheoryOfAlgorithms\Lab1\output\output.txt", true, System.Text.Encoding.Default))
             {
                 for (int i = 0; i < lines.Length; i++)
                 {
-                    for (int j = 0; j < lines[0].Split(' ').Length; j++)
+                    for (int j = 0; j < lines[0].Split().Length; j++)
                     {
-                        if(array[i, j] < min)
-                            min = array[i, j];
                         sw.Write(array[i, j] + " ");
                     }
                     sw.WriteLine();
                 }
-                sw.WriteLine("Min element of this array: " + min);
+                sw.WriteLine("Min element of this array: " + min + "\n" +
+                             "Index of min element: " + index + "\n" +
+                             "Time spent: " + stopWatch.ElapsedMilliseconds + " ms");
                 sw.WriteLine("----------------------------");
             }
             Console.WriteLine("--------------------------------------------\n" +
@@ -120,50 +154,70 @@ namespace Lab1
         //------------- Entering array from keyboard -------------
         static void EnterFromKeyboard()
         {
-            Console.WriteLine("-------------------------\n" +
-                              "Enter count of columns:");
-            int columns = int.Parse(Console.ReadLine());
-            Console.WriteLine("-------------------------\n" +
-                              "Enter count of rows:");
-            int rows = int.Parse(Console.ReadLine());
-            array = new int[rows, columns];
-            for (int i = 0; i < rows; i++)
+            Console.WriteLine("-------------------------\n" + "Enter count of columns:");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("-------------------------\n" + "Enter count of rows:");
+            int n = int.Parse(Console.ReadLine());
+            array = new int[n, m];
+            string index = "";
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < columns; j++)
+                for (int j = 0; j < m; j++)
                 {
                     Console.WriteLine("Enter the [" + i + ";" + j + "] element of array:");
                     array[i, j] = int.Parse(Console.ReadLine());
                 }
             }
             int min = array[0, 0];
-            Console.WriteLine("You entered this array:");
-            for (int i = 0; i < rows; i++)
+            Console.WriteLine("Array:");
+            PrintArray(array, m, n);
+            //Cheking time
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < columns; j++)
+                for (int j = 0; j < m; j++)
                 {
-                    Console.Write(array[i, j] + " ");
                     if (array[i, j] < min)
+                    {
                         min = array[i, j];
+                        index = "[" + i + "," + j + "]";
+                    }
                 }
-                Console.WriteLine();
+                
             }
+            stopWatch.Stop();
             Console.WriteLine("--------------------\n" +
                               "Min element: " + min + "\n" +
+                              "Index of min element: " + index + "\n" + 
+                              "Time spent: " + stopWatch.ElapsedMilliseconds + " ms\n" +
                               "--------------------\n");
             using (StreamWriter sw = new StreamWriter(@"C:\Users\ilyam\Desktop\Навчання\TheoryOfAlgorithms\Lab1\output\output.txt", true, System.Text.Encoding.Default))
             {
-                for (int i = 0; i < rows; i++)
+                for (int i = 0; i < n; i++)
                 {
-                    for (int j = 0; j < columns; j++)
+                    for (int j = 0; j < m; j++)
                     {
-                        if(array[i, j] < min)
-                            min = array[i, j];
                         sw.Write(array[i, j] + " ");
                     }
                     sw.WriteLine();
                 }
-                sw.WriteLine("Min element of this array: " + min);
+
+                sw.WriteLine("Min element of this array: " + min + "\n" +
+                             "Index of min element: " + index + "\n" +
+                             "Time spent: " + stopWatch.ElapsedMilliseconds + " ms");
                 sw.WriteLine("----------------------------");
+            }
+        }
+        public static void PrintArray(int[,] array, int m, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(array[i,j] + " ");
+                }
+                Console.WriteLine();
             }
         }
     }
