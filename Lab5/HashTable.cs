13017bf110561d168090935c3836006be845aef0
ilyam3004 <ilyamelnichuk3004@gmail.com>
2022-04-19 11:53:26 +0300

    Lab5

--- Lab5/HashTable.cs
+++ Lab5/HashTable.cs
@@ -7,7 +7,7 @@ namespace Lab5
     public class HashTable
     {
         private List<KeyValue<string, string>>[] array;
-        private const int DEF_CAPACITY = 10;
+        private const int DEF_CAPACITY = 5;
         private int capacity;
         private int size;
         public HashTable()
@@ -15,19 +15,18 @@ namespace Lab5
             capacity = DEF_CAPACITY;
             array = new List<KeyValue<string, string>>[capacity];
         }
-        public void Add(string key, string value) => AddNode(new KeyValue<string, string>(key, value), GetHash1(key));
+        public void Add(string key, string value) => AddNode(new KeyValue<string, string>(key, value), GetHash2(key));
         public void ShowAll()
         {
             for(int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine($"{i}:");
+                Console.Write($"{i} : ");
                 if (array[i] != null && array[i].Count != 0)
                 {
                     foreach (KeyValue<string, string> item in array[i])
-                    {
-                        Console.WriteLine($"\t{item.ToString()}");
-                    }
+                        Console.Write($"({item.ToString()}), ");
                 }
+                Console.WriteLine();
             }
         }
         public bool Remove(string key)
@@ -49,41 +48,39 @@ namespace Lab5
             }
             throw new Exception($"No value by this key - {key}");
         }
-
         public void Resize()
         {
-            List<KeyValue<string, string>> nodes = new();
+            List<KeyValue<string, string>>[] newArray = new List<KeyValue<string, string>>[capacity];
             foreach (List<KeyValue<string, string>> list in array)
             {
                 if (list != null)
                 {
                     foreach (KeyValue<string, string> item in list)
-                        nodes.Add(item);
+                    {
+                        int index = GetHash2(item.key);
+                        if (newArray[index] == null)
+                            newArray[index] = new List<KeyValue<string, string>>();
+                        newArray[index].Add(item); 
+                    }
                 }
             }
-            Array.Resize(ref array, size * 2);
-            for(int i = 0; i < array.Length; i++)
-                array[i] = null;
-            foreach (var item in nodes)
-                Add(item.key, item.value);
-            
+            array = newArray;
         }
-
         private void AddNode(KeyValue<string, string> node, int index)
         {
-            
             size++;
-            if((size * 100) / capacity > 80)
+            if ((size * 100) / capacity > 90)
             {
+                capacity = size * 2;
+                Resize();
             }
-
-            if (array[index] == null)
+            if(array[index] == null)
                 array[index] = new List<KeyValue<string, string>>();
             array[index].Add(node);
         }
         public string GetValue(string key) => GetNode(key).value;
         
-        private int GetHash1(string key) => Math.Abs(key.GetHashCode() % array.Length);
+        private int GetHash1(string key) => Math.Abs(key.GetHashCode() % capacity);
         private int GetHash2(string key) => (int) Math.Abs(key.Sum(x => char.GetNumericValue(x))) % capacity;
 
     }
--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -10,8 +10,14 @@ namespace Lab5
         static void Main(string[] args)
         {
             HashTable table = new();
-            table.Add("23.10.2000", "Name - Cake");
-            table.Add("23.5.2000", "Name1 - Cake");
+            table.Add("23.01.2004", "Sergiy - Cake");
+            table.Add("20.02.2004", "Demyan - Pancake");
+            table.Add("19.03.2004", "Ilya - Apple pie");
+            table.Add("15.04.2004", "Ivan - Brownie");
+            table.Add("19.07.2004", "Den - Blueberry pie");
+            table.Add("13.08.2004", "Igor - Сhocolate bar");
+            table.Add("20.05.2004", "Alex - Сake with frosting");
+            table.ShowAll();
         }
     }
 }
