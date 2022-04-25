21a591b139651507f1fdf26088af774d8a4a0c93
ilyam3004 <ilyamelnichuk3004@gmail.com>
2022-04-25 20:34:44 +0300

    Lab9

--- Lab9/Program.cs
+++ Lab9/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Channels;
 
 namespace Lab9
 {
@@ -7,17 +6,53 @@ namespace Lab9
     {
         static void Main(string[] args)
         {
-            int[] array = new int[10];
+            int[] fleet = new int[10];
             for(int i = 0; i < 10; i++)
-                array[i] = new Random().Next(0, 100);
-            
+                fleet[i] = new Random().Next(10, 99);
+
+            Console.WriteLine("--------------------------------------");
             Console.WriteLine("Start list of ships:");
-            Console.WriteLine($"{String.Join(", ", array)}");
-            Sorting.InsertionSort(ref array);
-            Console.WriteLine();
-            Console.WriteLine("After Sorting:");
-            foreach (var item in array)
-                Console.Write($"{item}, ");
+            Console.WriteLine($"{String.Join(", ", fleet)}");
+            
+            Sorting.BubleSort(ref fleet);
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Start list of ships after BubleSorting:");
+            Console.WriteLine($"{String.Join(", ", fleet)}");
+            
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Second fleet is arriving...");
+            Console.WriteLine("--------------------------------------");
+            
+            int[] secondFleet = new int[10];
+            for(int i = 0; i < 10; i++)
+                secondFleet[i] = new Random().Next(10, 99);
+            Array.Resize(ref fleet, 20);
+            secondFleet.CopyTo(fleet, 10);
+            Console.WriteLine("List of ships after arriving the second fleet:");
+            Console.WriteLine($"{String.Join(", ", fleet)}");
+            Console.WriteLine("------------------------------------------------------------------------------");
+            
+            Sorting.InsertionSort(ref fleet);
+            Console.WriteLine("List of ships after InsertingSort:");
+            Console.WriteLine($"{String.Join(", ", fleet)}");
+            Console.WriteLine("------------------------------------------------------------------------------");
+            
+            Console.WriteLine("Third fleet is arriving...");
+            Console.WriteLine("--------------------------------------");
+            
+            int[] thirdFleet = new int[10];
+            for(int i = 0; i < 10; i++)
+                thirdFleet[i] = new Random().Next(10, 99);
+            Array.Resize(ref fleet, 30);
+            thirdFleet.CopyTo(fleet, 20);
+            Console.WriteLine("List of ships after arriving the second fleet:");
+            Console.WriteLine($"{String.Join(", ", fleet)}");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            
+            Sorting.QuickSort(ref fleet, 0, fleet.Length - 1);
+            Console.WriteLine("List of ships after QuickSort:");
+            Console.WriteLine($"{String.Join(", ", fleet)}");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
             
         }
     }
